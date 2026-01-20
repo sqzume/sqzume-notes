bb90a68f89b8a27ac0951664372b220185f3bd82
sqzume <173809390+sqzume@users.noreply.github.com>
2026-01-20 22:33:07 +0900

    最近の投稿5つを表示する部分を追加

---
+++ src/content/config.ts
@@ -0,0 +1,11 @@
+import { defineCollection, z } from "astro:content";
+
+const posts = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+  }),
+});
+
+export const collections = { posts };
